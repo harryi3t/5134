982511928a43d3dd15350230a557da065ea1d3d8
Harendra Singh <66hari@gmail.com>
2017-03-26 13:55:30 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,23 +2,15 @@
 
 var hello = require('./hello.js');
 
-var names = [
-  'harry',
-  'barry',
-  'garry',
-  'harry',
-  'barry',
-  'marry',
-];
+var names = ['harry', 'barry', 'garry', 'harry', 'barry', 'marry'];
 
 // this line in unchanged
 
 // after this line new chunk will be created
 var names2 = [
   'harry',
-  'barry',
   'garry',
   'harry',
-  'barry',
+  'barry', 'barry',
   'marry',
 ];
