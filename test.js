0c45e257d3071a7e91d522de5319958217ee9338
Harendra Singh <66hari@gmail.com>
2017-03-26 13:54:05 +0530

    Update test.js

--- test.js
+++ test.js
@@ -10,3 +10,15 @@ var names = [
   'barry',
   'marry',
 ];
+
+// this line in unchanged
+
+// after this line new chunk will be created
+var names2 = [
+  'harry',
+  'barry',
+  'garry',
+  'harry',
+  'barry',
+  'marry',
+];
